563aaf7ffae5d59d59148cb47f4c9035f848d2c6
Jonathon Harrell <kaos78414@yahoo.com>
2014-11-29 17:00:59 -0600

    Remove e2e test suite

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,6 @@ module.exports = function (grunt) {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
     cdnify: 'grunt-google-cdn',
-    protractor: 'grunt-protractor-runner',
     injector: 'grunt-asset-injector',
     buildcontrol: 'grunt-build-control'
   });
@@ -465,19 +464,6 @@ module.exports = function (grunt) {
       src: ['server/**/*.spec.js']
     },
 
-    protractor: {
-      options: {
-        configFile: 'protractor.conf.js'
-      },
-      chrome: {
-        options: {
-          args: {
-            browser: 'chrome'
-          }
-        }
-      }
-    },
-
     env: {
       test: {
         NODE_ENV: 'test'
@@ -681,21 +667,6 @@ module.exports = function (grunt) {
         'injector',
         'autoprefixer',
         'karma'
-      ]);
-    }
-
-    else if (target === 'e2e') {
-      return grunt.task.run([
-        'clean:server',
-        'env:all',
-        'env:test',
-        'injector:stylus', 
-        'concurrent:test',
-        'injector',
-        'wiredep',
-        'autoprefixer',
-        'express:dev',
-        'protractor'
       ]);
     }
 
